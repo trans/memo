ce48594053e91a3c83460924622fd88e8e6dc85b
Thomas Sawyer <transfire@gmail.com>
2026-04-26 00:36:35 -0400

    Bump v0.12.0

--- src/memo.cr
+++ src/memo.cr
@@ -80,5 +80,5 @@ require "./memo/service"
 # Internal modules (Storage, Search, Chunking, RRF) remain accessible
 # for advanced use cases but Service is the recommended entry point.
 module Memo
-  VERSION = "0.11.7"
+  VERSION = "0.12.0"
 end
